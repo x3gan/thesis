fbf7beb9e91d068e1542e359243ac57a40183401
lilexuhr <v4asui@inf.elte.hu>
2025-04-16 11:08:19 +0200

    Setting up lsupdate for full lsdb exchange

--- lsdb.py
+++ lsdb.py
@@ -6,24 +6,46 @@ from scapy.packet import Packet
 
 class LSDB:
     def __init__(self):
-        self.lsa_db : dict[int, dict[tuple, Packet]] = {}
+        self.lsa_db : dict[int, dict[str, Packet]] = {}
 
     def add(self, lsa: Packet) -> None:
         """
         LSA hozzáadása az adatbázishoz (csak a legfrissebb verziót tároljuk)
         :param lsa: OSPF LSA csomag (OSPF_LSA_Hdr-t tartalmazó)
         """
-        lsa_hdr  = lsa[OSPF_LSA_Hdr]
-        lsa_type = lsa_hdr.type
-        key      = (lsa_hdr.id, lsa_hdr.adrouter)
+        lsa_type = lsa.type
+        lsa_adrouter = lsa.adrouter
+
+        key = lsa_adrouter
 
         if lsa_type not in self.lsa_db:
             self.lsa_db[lsa_type] = {}
 
         self.lsa_db[lsa_type][key] = lsa
 
-    def get(self, adrouter):
-        return self.lsa_db.get(adrouter, None)
+    def get(self, adrouter : str, lsa_type : int) -> Packet | None:
+        """
+        LSA lekérdezése az adatbázisból.
+        :param ad_id: A LSA
+        :param adrouter:
+        :param lsa_type:
+        :return:
+        """
+        if lsa_type not in self.lsa_db:
+            self.lsa_db[lsa_type] = {}
+
+        lsa = self.lsa_db[lsa_type].get(adrouter, None)
+        return lsa
 
     def get_all(self) -> list[Packet]:
-        return [lsa for lsa_type in self.lsa_db.values() for lsa in lsa_type.values()]
+        """
+        Visszaadja az összes LSA csomagot az adatbázisból.
+        :return: Az összes LSA csomag listája
+        """
+        packets = []
+
+        for type in self.lsa_db.values():
+            for packet in type.values():
+                packets.append(packet)
+
+        return packets
