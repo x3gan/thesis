1a268d74b11bbbbe96dc7e0a003085bb513291e4
lilexuhr <v4asui@inf.elte.hu>
2025-04-24 00:10:45 +0200

    Adding main back to ospf bc im silly and deleted it

--- ospf_core/ospf.py
+++ ospf_core/ospf.py
@@ -585,19 +585,19 @@ class OSPF:
                 logging.warning(f"A {thread.name} szál nem állt le biztonságosan.")
 
 
-# if __name__ == '__main__':
-#     router_name       = sys.argv[1]
-#     info_logger       = InfoLogger(name= router_name, log_dir= INFO_LOG_DIR)
-#     network_interface = ScapyInterface()
-#
-#     ospf = OSPF(router_name, CONFIG_PATH, network_interface, info_logger)
-#
-#     try:
-#         ospf.start()
-#         while True:
-#             sleep(0.5)
-#     except KeyboardInterrupt:
-#         print("\nLeállítási parancsot kapott... (CTRL + C)")
-#         ospf.stop()
-#     finally:
-#         print("Az OSPF futása leállt.")
+if __name__ == '__main__':
+    router_name       = sys.argv[1]
+    info_logger       = InfoLogger(name= router_name, log_dir= INFO_LOG_DIR)
+    network_interface = ScapyInterface()
+
+    ospf = OSPF(router_name, CONFIG_PATH, network_interface, info_logger)
+
+    try:
+        ospf.start()
+        while True:
+            sleep(0.5)
+    except KeyboardInterrupt:
+        print("\nLeállítási parancsot kapott... (CTRL + C)")
+        ospf.stop()
+    finally:
+        print("Az OSPF futása leállt.")
